65a3232cdb118585c54590f04ea75fff0bd08849
chongqingxu <firefoxdance@163.com>
2016-01-26 15:50:10 +0800

    Initial Commit

---
+++ MarrayMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarrayMe
+//
+//  Created by 崇庆旭 on 16/1/26.
+//  Copyright © 2016年 崇庆旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
